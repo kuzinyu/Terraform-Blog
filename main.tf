59dcce0c3182829b7319d09b0d5af0a038372f60
Vitaly Zverev <vitaly.zverev@gmail.com>
2023-06-06 21:28:22 +0300

    Adopt for yandex cloud

--- main.tf
+++ main.tf
@@ -1,47 +1,155 @@
-provider "aws" {
-  region     = "us-east-1"
-  
-  
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+    }
+  }
+  required_version = ">= 0.13"
 }
 
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_public" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.ssh-security-group.id}"]
-  subnet_id                   = aws_subnet.public-subnet-1.id
-  associate_public_ip_address = true
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "EC2-PUBLIC"
-  }
-  
- 
-}
-#Create a new EC2 launch configuration
-resource "aws_instance" "ec2_private" {
-  ami                         = var.ami_id
-  instance_type               = var.instance_type
-  key_name                    = var.key_name
-  security_groups             = ["${aws_security_group.webserver-security-group.id}"]
-  subnet_id                   = aws_subnet.private-subnet-1.id
-  associate_public_ip_address = false
-  #user_data                   = "${data.template_file.provision.rendered}"
-  #iam_instance_profile = "${aws_iam_instance_profile.some_profile.id}"
-  lifecycle {
-    create_before_destroy = true
-  }
-  tags = {
-    "Name" = "EC2-PRIVATE"
-  }
- 
-}
-################################
- 
+variable "token" {}
+variable "cloud_id" {}
+variable "folder_id" {}
+variable "zone" {}
 
+provider "yandex" {
+
+  token                    =  var.token  
+  cloud_id                 =  var.cloud_id  
+  folder_id                =  var.folder_id 
+  zone                     =  var.zone 
+}
+
+
+resource "yandex_dataproc_cluster" "foo" {
+  depends_on = [yandex_resourcemanager_folder_iam_binding.dataproc]
+
+  bucket      = yandex_storage_bucket.foo.bucket
+  description = "Dataproc Cluster created by Terraform"
+  name        = "dataproc-cluster"
+  labels = {
+    created_by = "terraform"
+  }
+  service_account_id = yandex_iam_service_account.dataproc.id
+  zone_id            = "ru-central1-a"
+
+  cluster_config {
+    # Certain cluster version can be set, but better to use default value (last stable version)
+    # version_id = "2.0"
+
+    hadoop {
+      #services = ["HDFS", "YARN", "SPARK", "TEZ", "MAPREDUCE", "HIVE"]
+      services = ["HDFS", "YARN", "SPARK"]
+      properties = {
+        "yarn:yarn.resourcemanager.am.max-attempts" = 5
+      }
+      ssh_public_keys = [
+      file("~/.ssh/id_rsa.pub")]
+    }
+
+    subcluster_spec {
+      name = "main"
+      role = "MASTERNODE"
+      resources {
+        resource_preset_id = "s2.small"
+        disk_type_id       = "network-hdd"
+        disk_size          = 20
+      }
+      subnet_id   = yandex_vpc_subnet.foo.id
+      hosts_count = 1
+    }
+
+    subcluster_spec {
+      name = "data"
+      role = "DATANODE"
+      resources {
+        resource_preset_id = "s2.small"
+        disk_type_id       = "network-hdd"
+        disk_size          = 20
+      }
+      subnet_id   = yandex_vpc_subnet.foo.id
+      hosts_count = 2
+    }
+
+    subcluster_spec {
+      name = "compute"
+      role = "COMPUTENODE"
+      resources {
+        resource_preset_id = "s2.small"
+        disk_type_id       = "network-hdd"
+        disk_size          = 20
+      }
+      subnet_id   = yandex_vpc_subnet.foo.id
+      hosts_count = 2
+    }
+
+    subcluster_spec {
+      name = "compute_autoscaling"
+      role = "COMPUTENODE"
+      resources {
+        resource_preset_id = "s2.small"
+        disk_type_id       = "network-hdd"
+        disk_size          = 20
+      }
+      subnet_id   = yandex_vpc_subnet.foo.id
+      hosts_count = 2      
+      autoscaling_config {
+        max_hosts_count = 10
+        measurement_duration = 60
+        warmup_duration = 60
+        stabilization_duration = 120
+        preemptible = false
+        decommission_timeout = 60
+      }
+    }
+  }
+}
+
+resource "yandex_vpc_network" "foo" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  zone           = "ru-central1-a"
+  network_id     = yandex_vpc_network.foo.id
+  v4_cidr_blocks = ["10.1.0.0/24"]
+}
+
+resource "yandex_iam_service_account" "dataproc" {
+  name        = "dataproc"
+  description = "service account to manage Dataproc Cluster"
+}
+
+data "yandex_resourcemanager_folder" "foo" {
+  folder_id = "b1goq1sgaqo50ok2drfn"
+}
+
+resource "yandex_resourcemanager_folder_iam_binding" "dataproc" {
+  folder_id = data.yandex_resourcemanager_folder.foo.id
+  role      = "dataproc.agent"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.dataproc.id}",
+  ]
+}
+
+// required in order to create bucket
+resource "yandex_resourcemanager_folder_iam_binding" "bucket-creator" {
+  folder_id = data.yandex_resourcemanager_folder.foo.id
+  role      = "editor"
+  members = [
+    "serviceAccount:${yandex_iam_service_account.dataproc.id}",
+  ]
+}
+
+resource "yandex_iam_service_account_static_access_key" "foo" {
+  service_account_id = yandex_iam_service_account.dataproc.id
+}
+
+resource "yandex_storage_bucket" "foo" {
+  depends_on = [
+    yandex_resourcemanager_folder_iam_binding.bucket-creator
+  ]
+
+  bucket     = "foo"
+  access_key = yandex_iam_service_account_static_access_key.foo.access_key
+  secret_key = yandex_iam_service_account_static_access_key.foo.secret_key
+}
 
