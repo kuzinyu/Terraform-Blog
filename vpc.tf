29f82c317a470d2316220ad4a0f032777d0d4d05
Vitaly Zverev <vitaly.zverev@gmail.com>
2023-06-06 22:41:35 +0300

    Update bucket name

--- vpc.tf
+++ vpc.tf
@@ -125,7 +125,7 @@ resource "yandex_storage_bucket" "foo" {
     yandex_resourcemanager_folder_iam_binding.bucket-creator
   ]
 
-  bucket     = "foo"
+  bucket     = "dataproc_shared_bucket"
   access_key = yandex_iam_service_account_static_access_key.foo.access_key
   secret_key = yandex_iam_service_account_static_access_key.foo.secret_key
 }
